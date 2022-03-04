77a3092a1b4235092e2cd512285afa6160f8af34
Mimi Majgaard <mimimajgaard@gmail.com>
2022-03-04 14:36:53 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(catName) {
+    cats.push(catName);
+}
+function destructivelyPrependCat(catName) {
+    cats.unshift(catName);
+}
+function destructivelyRemoveLastCat(lastCat) {
+    cats.pop(lastCat);
+}
+function destructivelyRemoveFirstCat(firstCat) {
+    cats.shift(firstCat);
+}
+
+function appendCat(catName){
+return [...cats, "Broom"];
+
+}
+
+function prependCat(catName){
+    return ["Arnold", ...cats];
+    
+    }
+
+function removeLastCat(catName){
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat(catName){
+    return cats.slice(1);
+}
+
+//1. Find what is the array - it is cats
+//2. Google: what is console log examples javascript
+//10 - 20 min - look at the top 3 links
+//3. Use console.log to see what catName is
+//4. You will put the array(cats) in place of catName
+//5. You will put in catName some place
